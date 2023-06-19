5269f8635841884cf969c046088f936c93712313
mblanco81 <127463754+mblanco81@users.noreply.github.com>
2023-06-18 22:00:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+//console.log(document.body);
+
+// getelement por ID
+// let div = document.getElementById("aplicacion");
+// console.log(div.innerHTML);
+// console.log(div.innerText);
+
+// let p = document.getElementById("parrafo");
+// console.log(div.innerHTML);
+// console.log(div.innerText);
+
+// //getelementbyClassName
+
+
+// let contenedor = document.getElementById("contenedor")
+
+// let parrafo = document.createElement("p");
+// parrafo.innerHtml = "<h1>Hola Muchachos</h1>";
+// document.body.append(parrafo)
+
+// let nombres = ["juan","camila","pepito"]
+
+// nombres.forEach(item => {
+//     let div =document.createElement("div")
+//     div.innerHTML = item;
+//     contenedor.append(div);
+// });
+
+let contenedor = document.getElementById("contenedor");
+const productos =[
+    {id:,nombre:"camisa",precio:1000},
+    {id:,nombre:"gorra",precio:1000},
+    {id:,nombre:"zapato",precio:1000},
+    {id:,nombre:"medias",precio:1000},
+]
